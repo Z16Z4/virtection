834269f1f566d1a5a9c88628fcdc0bf137aae83e
cronos-hash <apollo@dnmx.org>
2021-02-22 23:50:16 +0000

    binary checks for rdtsc

--- rdtsc.c
+++ rdtsc.c
@@ -4,14 +4,19 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include<conio.h>
+/*variable for rdtsc check*/
 int rdtsc_check();
+/*variable for vmexit*/
 int vmexit_check();
+/* inline usigned long long rdtscd >> ret1 & 2*/
 static inline unsigned long long rdtscd() {
 	unsigned long long ret, ret2;
 	unsigned eax, edx;
 	__asm__ volatile("rdtsc" : "=a" (eax), "=d" (edx));
+    /*pipe ret binary left shift 32*/
 	ret  = ((unsigned long long)eax) | (((unsigned long long)edx) << 32);
 	__asm__ volatile("rdtsc" : "=a" (eax), "=d" (edx));
+    /*pipe into ret 2 binary left shift 32*/
 	ret2  = ((unsigned long long)eax) | (((unsigned long long)edx) << 32);
 	return ret2 - ret;
 }
@@ -20,27 +25,31 @@ static inline unsigned long long rdtscd_vmexit() {
 	unsigned eax, edx;
 	__asm__ volatile("rdtsc" : "=a" (eax), "=d" (edx));
 	ret  = ((unsigned long long)eax) | (((unsigned long long)edx) << 32);
-
+    /*cpuid 0x00 no output*/
 	__asm__ volatile("cpuid" : /* no output */ : "a"(0x00));
-
 	__asm__ volatile("rdtsc" : "=a" (eax), "=d" (edx));
+    /*ret 2 pipe binaries till left shift 32*/
 	ret2  = ((unsigned long long)eax) | (((unsigned long long)edx) << 32);
 	return ret2 - ret;
 }
 int rdtsc_check() {
 	int i;
+    /*average starts 0*/
 	unsigned long long average = 0;
 	for (i = 0; i < 10; i++) {
+        /*adds first rdtsc process*/
 		average = average + rdtscd();
 		Sleep(500);
 	}
 	average = average / 10;
+    /*returns false if average less than 750 true average 0*/
 	return (average < 750 && average > 0) ? FALSE : TRUE;
 }
 int vmexit_check() {
 	int i;
 	unsigned long long average = 0;
 	for (i = 0; i < 10; i++) {
+        /*repeat process for vmexit checks*/
 		average = average + rdtscd_vmexit();
 		Sleep(500);
 	}
