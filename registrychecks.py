c2965d4c7fcf2fc04ee608208ac828225cc280e2
cronos-hash <apollo@dnmx.org>
2021-02-22 01:37:14 +0000

    detecting vm drivers

--- registrychecks.py
+++ registrychecks.py
@@ -23,27 +23,25 @@ def registrysearch(registry, query, queryresult, string, type):
         print(colored(type+ ": Detected", 'red'))
     else:
         #'virtual machine not detected
-        print(type + ": Passed")
+        print(colored(type + ": Passed", 'green'))
     #debugging
-    print(colored("debug info" + queryresult, 'green'))
+    print(colored("debug info" + queryresult, 'yellow'))
 
 def registryindex(registry, string, type):
     #accessing registry through init HKEY
     access_registry = winreg.ConnectRegistry(None,winreg.HKEY_LOCAL_MACHINE)
     #opening registry 
     access_key = winreg.OpenKey(access_registry,registry)
-    for i in range(20):
+    for i in range(1028):
         try:
             #results based on directory names
             x =winreg.EnumKey(access_key,i)
             if x == string:
                 print(colored(type+ ": Detected", 'red'))
-            else:
-                print(type+ ": Passed")
         except:
             break
     #debugging
-    print(colored("debug info " + x, 'green'))
+    print(colored("debug info " + x, 'yellow'))
 
 a = 'null'
 registrysearch(r"SYSTEM\HardwareConfig", "BIOSVendor", a, "Development Kit", "OVMF Check(0)")
@@ -51,3 +49,6 @@ registrysearch(r"SYSTEM\HardwareConfig", "BIOSVendor", a, "OVMF", "OVMF Check(1)
 registrysearch(r"SYSTEM\HardwareConfig", "SystemProductName", a, "Q35", "O35 Check(0)")
 registrysearch(r"SYSTEM\HardwareConfig", "SystemVersion", a, "pc-q35", "Q35 Check(1)")
 registryindex(r"SOFTWARE\WOW6432Node\RedHat", "RHEL", "RedHat Check(0)")
+registrysearch(r"SYSTEM\DriverDatabase\DriverPackages", "Provider", a, "Red Hat", "RedHat Check (1)")
+##registryindex(r"SYSTEM\DriverDatabase\DriverPackages", "virtdisk", "RedHat Driver Check(1)")
+
